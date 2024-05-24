d067405ee06c419fcd331086418c896f7faf5fe9
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2024-05-24 15:28:25 -0700

    add helmet and google fonts

--- src/Components/Header/Index.tsx
+++ src/Components/Header/Index.tsx
@@ -4,28 +4,38 @@ type HeaderProps = {}
 
 const Header = ({ }: HeaderProps) => {
   return (
-    <div className='w-full top-0 left-0 w-360 h-800 fixed'>
+    <div className='w-full top-0 left-0 w-360 h-800 fixed bg-sp-background-300'>
       <TypeAnimation
         sequence={[
           // Same substring at the start will only be typed out once, initially
-          'Software Developer',
-          1500, // wait 1s before replacing "Mice" with "Hamsters"
-          'Software Engineer',
+          'Logan The Front-End Developer',
           1500,
-          'Software Coder',
+          'Logan The Front-End Engineer',
           1500,
-          'Software Enjoyer',
+          'Logan The Cloud Engineer',
           1500,
-          'Rock Climber',
+          'Logan The Web Developer',
           1500,
-          'Video Gamer',
+          'Logan The Full-Stack Engineer',
           1500,
-          'Cat Enjoyer',
+          'Logan The Full-Stack Developer',
           1500,
+          'Logan The Rock Climber',
+          1500,
+          'Logan The Cat Enjoyer',
+          1500,
+          'Logan The Bald Guy',
+          1500,
+          'Logan The Cook',
+          1500,
+          // 'Logan The ',
+          // 1500,
+          // 'Logan The ',
+          // 1500,
         ]}
-        wrapper="span"
+        wrapper="h1"
         speed={50}
-        style={{ fontSize: '2em', display: 'inline-block' }}
+        style={{ fontSize: '3rem', lineHeight: '3.4rem', display: 'inline-block' }}
         repeat={Infinity}
       />
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import Home from './Pages/Home/';
 import { Error } from './Pages/Error';
 import { useState } from 'react';
 import MobileNav from './Components/MobileNav';
+import { Helmet } from 'react-helmet';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -20,6 +21,11 @@ const Layout = () => {
 
   return (
     <>
+      <Helmet>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+      </Helmet>
       <Outlet />
       <MobileNav open={open} handleOpen={handleMobileNav} />
     </>
