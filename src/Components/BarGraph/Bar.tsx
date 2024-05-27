cdbc66d7f765cedd83a201776bbcae7663adf57e
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2024-05-26 17:06:31 -0700

    wip skills bargraph

--- src/Components/BarGraph/Bar.tsx
+++ src/Components/BarGraph/Bar.tsx
@@ -9,13 +9,33 @@ type BarProps = {
 
 const Bar = ({ dataPoint }: BarProps) => {
   let val = "";
+  let text = "";
   switch (dataPoint.value) {
     case 1:
-      val = "w-1/4 bg-"
+      val = "w-1/4 bg-sp-contrast-orange-400"
+      text = 'Learning'
+      break
+    case 2:
+      val = 'w-1/5 bg-sp-contrast-yellow-400'
+      text = 'Familiar'
+      break
+    case 3:
+      val = 'w-3/4 bg-sp-contrast-blue-500'
+      text = 'Experienced'
+      break
+    case 4:
+      val = 'w-full bg-sp-contrast-green-400'
+      text = 'Proficient'
+      break
+    default:
+      text = 'error'
   }
 
   return (
     <div>
+      <div>
+
+      </div>
 
     </div>
   )
