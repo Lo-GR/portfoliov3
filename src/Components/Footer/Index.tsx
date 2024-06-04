53b2ed418a285588b7c647490482e8452002dc8c
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2024-06-03 21:55:20 -0700

    fix linting error

--- src/Components/Footer/Index.tsx
+++ src/Components/Footer/Index.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+
 import MainContainer from '../MainContainer/Index'
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import { Email, GitHub } from '@mui/icons-material';
+import { GitHub } from '@mui/icons-material';
 
 type FooterProps = {
   footerContent: {
